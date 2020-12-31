5816b66f83df2e9df577202cc0cf8b6cbddf6d2f
duyhung0311 <56124429+duyhung0311@users.noreply.github.com>
2020-12-31 13:28:39 +0700

    cms web

---
+++ coreui-free-react-admin-template-master/src/containers/_nav.js
@@ -0,0 +1,15 @@
+export default [
+  {
+    _tag: 'CSidebarNavItem',
+    name: 'Product',
+    to: '/product',
+    icon: 'cil-drop',
+  },
+  {
+    _tag: "CSidebarNavItem",
+    name: "Category",
+    to: "/category",
+    icon: "cil-layers",
+  },
+]
+
